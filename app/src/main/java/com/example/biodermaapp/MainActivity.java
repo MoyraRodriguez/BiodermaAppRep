4fad7495b5a8c3c201233be80fd112833101ff01
Gapa <gapaqueteimporta@gmail.com>
2019-08-14 17:24:43 -0500

    see

--- app/src/main/java/com/example/biodermaapp/MainActivity.java
+++ app/src/main/java/com/example/biodermaapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //text
-        //puto el que lo lea 
+        //puto el que lo lea
     }
 }
