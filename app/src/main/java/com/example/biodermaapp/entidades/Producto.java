385c13ddda813f5c951710f8fe743ac988e9a883
MoyraRodriguez <moyrarodriguez@gmail.com>
2019-08-14 18:42:32 -0500

    Creada la clase producto

---
+++ app/src/main/java/com/example/biodermaapp/entidades/Producto.java
@@ -0,0 +1,15 @@
+package com.example.biodermaapp.entidades;
+
+public class Producto {
+
+    private String nombreProducto;
+    private String urlImagen;
+    private int imagen;
+    private String indicaciones;
+    private String beneficios;
+    private String informacion;
+    private String formato;
+
+
+
+}
