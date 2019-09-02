4aac10ff250c92526da17b17134215f0cc504d79
MoyraRodriguez <moyrarodriguez@gmail.com>
2019-09-02 10:51:53 -0500

    cambios

--- app/src/main/java/com/example/biodermaapp/activities/Producto.java
+++ app/src/main/java/com/example/biodermaapp/activities/Producto.java
@@ -19,21 +19,17 @@ public class Producto extends AppCompatActivity {
     public static TextView nombreProducto, dataProducto, consejos,nombreComplementario;
     public static ImageView imagenProducto, productoComplementario;
     public static Button buttonProductoComplementario;
-    private static Button buttonInfo,buttonBeneficios,buttonInformacion, buttonFormato;
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
-        buttonInfo = findViewById(R.id.buttonInfoProducto);
-        buttonBeneficios = findViewById(R.id.buttonBenefProducto);
-        buttonFormato = findViewById(R.id.buttonFormatProducto);
-        buttonInformacion = findViewById(R.id.buttonInfoProducto);
+
         buttonProductoComplementario = findViewById(R.id.buttonProductoComplementario);
         nombreProducto = findViewById(R.id.tvNombreProducto);
         nombreComplementario = findViewById(R.id.tvNombreProductoComplementario);
-        dataProducto = findViewById(R.id.tvProductoData);
+
         consejos = findViewById(R.id.tvConsejosUso);
         imagenProducto = findViewById(R.id.imageProducto);
         productoComplementario = findViewById(R.id.imageProductoComplementario);
@@ -49,7 +45,7 @@ public class Producto extends AppCompatActivity {
 
         nombreProducto.setText( intent.getExtras().getString("Nombre"));
         imagenProducto.setImageResource(intent.getExtras().getInt("Imagen"));
-        dataProducto.setText(intent.getExtras().getString("Descripcion"));
+        //dataProducto.setText(intent.getExtras().getString("Descripcion"));
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
